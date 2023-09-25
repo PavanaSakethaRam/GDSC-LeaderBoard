09bfb49ede8cea0410d69fc4786a9f0a31a5d40e
shishiro <dheervathshishiro@gmail.com>
2023-09-25 11:29:32 +0530

    Update: Home Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {
-      colors: {
-        white: "#fff",
-      },
-      spacing: {},
-      fontFamily: {
-        roboto: "Roboto",
-        "m-plus-1": "'M PLUS 1'",
-      },
-    },
-    fontSize: {
-      "5xl": "24px",
-      "21xl": "40px",
-      inherit: "inherit",
-    },
-  },
-  corePlugins: {
-    preflight: false,
-  },
-};
+    extend: {},
+    plugins: [],
+  }
+}
